fa3a8ccb8bf84a751a56043bf446a72b141045c6
Esteban Vergara <esteban_lcd@hotmail.com>
2024-11-21 21:22:35 -0500

    Bugfix

--- src/lib/utils/sortFunctions.ts
+++ src/lib/utils/sortFunctions.ts
@@ -1,10 +1,6 @@
 // sort by date
 export const sortByDate = (array: any[]) => {
-  const sortedArray = array.sort(
-    (a:any, b:any) =>
-      new Date(b.data.date && b.data.date) -
-      new Date(a.data.date && a.data.date)
-  );
+  const sortedArray = array.sort();
   return sortedArray;
 };
 
